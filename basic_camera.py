2cf834d1df856aee718e957b4f83db4f43f0f2f8
Jackie-N0307 <140470423+Jvckiee@users.noreply.github.com>
2024-11-27 16:52:03 +1100

    Mediapipe working

--- basic_camera.py
+++ basic_camera.py
@@ -2,8 +2,47 @@ import cv2
 import mediapipe as mp
 from mediapipe.tasks import python
 from mediapipe.tasks.python import vision
+from mediapipe import solutions
+from mediapipe.framework.formats import landmark_pb2
+import numpy as np
+import PIL as Image
+
+
 # Open the default camera
+
+
+
 cam = cv2.VideoCapture(0)
+
+model_path = r'pose_landmarker_full.task'
+
+# STEP 2: Create an PoseLandmarker object.
+base_options = python.BaseOptions(model_path)
+options = vision.PoseLandmarkerOptions(
+    base_options=base_options,
+    output_segmentation_masks=True)
+detector = vision.PoseLandmarker.create_from_options(options)
+
+def draw_landmarks_on_image(rgb_image, detection_result):
+  pose_landmarks_list = detection_result.pose_landmarks
+  annotated_image = np.copy(rgb_image)
+
+  # Loop through the detected poses to visualize.
+  for idx in range(len(pose_landmarks_list)):
+    pose_landmarks = pose_landmarks_list[idx]
+
+    # Draw the pose landmarks.
+    pose_landmarks_proto = landmark_pb2.NormalizedLandmarkList()
+    pose_landmarks_proto.landmark.extend([
+      landmark_pb2.NormalizedLandmark(x=landmark.x, y=landmark.y, z=landmark.z) for landmark in pose_landmarks
+    ])
+    solutions.drawing_utils.draw_landmarks(
+      annotated_image,
+      pose_landmarks_proto,
+      solutions.pose.POSE_CONNECTIONS,
+      solutions.drawing_styles.get_default_pose_landmarks_style())
+  return annotated_image
+
 
 # Get the default frame width and height
 frame_width = int(cam.get(cv2.CAP_PROP_FRAME_WIDTH))
@@ -16,11 +55,26 @@ out = cv2.VideoWriter('output.mp4', fourcc, 20.0, (frame_width, frame_height))
 while True:
     ret, frame = cam.read()
 
-    # Write the frame to the output file
-    #out.write(frame)
+    #print(frame,"frame")
+    
+    
+    # STEP 3: Load the input image.
+    #image = mp.Image.create_from_file(frame)
+    mp_image = mp.Image(image_format=mp.ImageFormat.SRGB, data=frame)
 
+    rgb_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+
+    # STEP 4: Detect pose landmarks from the input image.
+    detection_result = detector.detect(mp_image)
+
+    # STEP 5: Process the detection result. In this case, visualize it.
+    annotated_image = draw_landmarks_on_image(mp_image.numpy_view(), detection_result)
+    
+    
+    cv2.imshow("Camera",annotated_image)
+    #cv2.imshow("Camera",cv2.cvtColor(annotated_image, cv2.COLOR_RGB2BGR))
     # Display the captured frame
-    cv2.imshow('Camera', frame)
+   #cv2.imshow('Camera', frame)
 
     # Press 'q' to exit the loop
     if cv2.waitKey(1) == ord('q'):
